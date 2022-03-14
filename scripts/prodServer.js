99119282918fb6308d263c49fbe303e1e21d3872
kailash goswami <kailash.goswami@gmail.com>
2022-03-14 22:38:04 +0530

    proxy setup

--- scripts/prodServer.js
+++ scripts/prodServer.js
@@ -1,17 +1,20 @@
 const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
+const apiUrl = process.env.API_URL || 'https://dry-scrubland-97450.herokuapp.com';
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
+app.use(favicon(__dirname + '/../build/favicon.ico'));
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+app.use(express.static(path.join(__dirname, '../build')));
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
-
-app.listen(port);
+app.use('/api', createProxyMiddleware({
+  target: apiUrl,
+  changeOrigin: true,
+}));
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
